5f81d06f2021eba9f635ab910220ff9ffdabdc9c
kuro-water <kuro07001@gmail.com>
2025-01-07 18:59:00 +0900

    継承でいい感じに共通化

--- app/src/main/java/com/example/layout/Layouts/AirConditionerLayout.kt
+++ app/src/main/java/com/example/layout/Layouts/AirConditionerLayout.kt
@@ -7,168 +7,20 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.layout.EchonetLiteManager
+import com.example.layout.EchonetLiteObject
 import com.example.layout.ListAdapter
 import com.example.layout.ListItem
 import com.example.layout.R
 import kotlinx.coroutines.launch
 
-class AirConditionerLayout : AppCompatActivity(), ListAdapter.OnSwitchClickListener {
-
-    private var data = mutableListOf<ListItem>()
-    private lateinit var recycView: RecyclerView
-    private lateinit var adapter: ListAdapter
-
-
-    private fun updateData(position: Int, isOn: Boolean) {
-        data[position].Switch = isOn
-        data[position].SwitchState = if (isOn) "ON" else "OFF"
-        recycView.post { // ここでも変更するためpostを使用
-            adapter.notifyItemChanged(position) //
-        }
-    }
-
-    private fun showData() {
-        for (i in 0..<data.size) {
-            data.removeAt(0)
-//            recycView.post {
-//                adapter.notifyItemRemoved(0)
-//            }
-        }
-        for (i in 0..<EchonetLiteManager.deviceList.size) {
-            // エアコン以外はいらない
-            if (!(EchonetLiteManager.deviceList[i].compareEoj(listOf(0x01, 0x30)))) {
-                continue
-            }
-            val mainText = EchonetLiteManager.deviceList[i].name["en"].toString()
-            val subText = EchonetLiteManager.deviceList[i].ipAddress.toString()
-            val isOn = EchonetLiteManager.deviceList[i].status[0x80.toByte()] == 0x30.toByte()
-//            addData(i, mainText, subText, isOn)
-            data.add(ListItem(i, mainText, subText, isOn, if (isOn) "ON" else "OFF"))
-        }
-        println("bbbbb")
-        recycView.post {
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-
-    private fun checkData() {
-        lifecycleScope.launch {
-            for (i in 0..<data.size) {
-                val obj = EchonetLiteManager.deviceList[data[i].id]
-                val sent = obj.asyncGet("動作状態")
-                if (sent == null) {
-                    println("sent is null")
-                    continue
-                }
-                val ret = EchonetLiteManager.asyncWaitPacket(sent)
-                if (ret == null) {
-                    println("ret is null")
-                    continue
-                }
-                if (ret.edt.isNullOrEmpty()) {
-                    println("edt is null")
-                    continue
-                }
-                val edt = obj.edtToString(ret.epc, ret.edt)
-                updateData(i, edt == "true")
+class AirConditionerLayout : DefaultLayout() {
+    override fun isTargetEoj(obj: EchonetLiteObject<Number>): Boolean {
+        val eojList = listOf((listOf(0x01, 0x30)))
+        for (eoj in eojList) {
+            if (obj.compareEoj(eoj)) {
+                return true
             }
         }
-    }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_elect)
-//        val genebutton = findViewById<Button>(R.id.inc_button)
-        recycView = findViewById<RecyclerView>(R.id.rccv)
-        recycView.setHasFixedSize(true)
-        recycView.layoutManager = LinearLayoutManager(this).apply {
-            orientation = LinearLayoutManager.VERTICAL
-        }
-        adapter = ListAdapter(data, this)
-        recycView.adapter = adapter
-
-//
-//        genebutton.setOnClickListener {
-//            data.add(ListItem(4, "MainText4", "SubText4", false, "OFF"))
-//            recycView.post {//adapterをいじる際にpostを使用
-//                adapter.notifyItemInserted(data.size - 1)
-//            }
-//        }
-//
-//        fun onButtonClick(view: View) {
-//            finish()
-//        }
-
-        lifecycleScope.launch {
-            EchonetLiteManager.asyncGetDeviceList()
-            showData()
-            println("data:${data.size}件")
-            println(data)
-            checkData()
-        }
-    }
-
-    override fun onSwitchClick(position: Int, isChecked: Boolean) {
-        println("on press")
-        fun reset() {
-            updateData(position, data[position].Switch)
-        }
-
-        val id = data[position].id
-        val obj = EchonetLiteManager.deviceList[id]
-        lifecycleScope.launch {
-            // edt:"true" or "false"
-            val set = obj.asyncSetC("動作状態", isChecked.toString())
-            if (set == null) {
-                reset()
-                return@launch
-            }
-            EchonetLiteManager.asyncWaitPacket(set)
-
-            val get = obj.asyncGet("動作状態")
-            if (get == null) {
-                reset()
-                return@launch
-            }
-
-            val ret = EchonetLiteManager.asyncWaitPacket(get)
-            if (ret?.edt == null || ret.edt.isEmpty()) {
-                reset()
-                return@launch
-            }
-
-            println("this is ${obj.edtToString(ret.epc, ret.edt)}")
-            if (obj.edtToString(ret.epc, ret.edt) == isChecked.toString()) {
-                // ちゃんと返答が返ってきてるなら変更
-                updateData(position, isChecked)
-            } else {
-                reset()
-            }
-        }
-
-//        data[position].Switch = isChecked
-//        data[position].MainText = "Init"
-//        if (isChecked) {
-//            data[position].SwitchState = "ON"
-//        } else {
-//            data[position].SwitchState = "OFF"
-//        }
-//        recycView.post { // ここでも変更するためpostを使用
-//            adapter.notifyItemChanged(position) //
-//        }
-    }
-
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        super.onSaveInstanceState(savedInstanceState)
-        savedInstanceState.putParcelableArrayList("data", ArrayList<Parcelable>(data))
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        data.clear()
-        data.addAll(savedInstanceState.getParcelableArrayList<ListItem>("data")!!)
-        adapter.notifyDataSetChanged()
+        return false
     }
 }
--- app/src/main/java/com/example/layout/Layouts/ElseLayout.kt
+++ app/src/main/java/com/example/layout/Layouts/ElseLayout.kt
@@ -7,150 +7,21 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.layout.EchonetLiteManager
+import com.example.layout.EchonetLiteObject
 import com.example.layout.ListAdapter
 import com.example.layout.ListItem
 import com.example.layout.R
 import kotlinx.coroutines.launch
 
-class ElseLayout : AppCompatActivity(), ListAdapter.OnSwitchClickListener {
-
-    private var data = mutableListOf<ListItem>()
-    private lateinit var recycView: RecyclerView
-    private lateinit var adapter: ListAdapter
-
-
-    private fun updateData(position: Int, isOn: Boolean) {
-        data[position].Switch = isOn
-        data[position].SwitchState = if (isOn) "ON" else "OFF"
-        recycView.post { // ここでも変更するためpostを使用
-            adapter.notifyItemChanged(position) //
-        }
-    }
-
-    private fun showData() {
-        for (i in 0..<data.size) {
-            data.removeAt(0)
-//            recycView.post {
-//                adapter.notifyItemRemoved(0)
-//            }
-        }
-        for (i in 0..<EchonetLiteManager.deviceList.size) {
-            // エアコンと照明とNodeProfile以外
-            if (EchonetLiteManager.deviceList[i].compareEoj(listOf(0x01, 0x30))
-                || EchonetLiteManager.deviceList[i].compareEoj(listOf(0x02, 0x90))
-                || EchonetLiteManager.deviceList[i].compareEoj(listOf(0x02, 0x91))
-                || EchonetLiteManager.deviceList[i].compareEoj(listOf(0x0E, 0xF0))
-            ) {
-                continue
-            }
-            val mainText = EchonetLiteManager.deviceList[i].name["en"].toString()
-            val subText = EchonetLiteManager.deviceList[i].ipAddress.toString()
-            val isOn = EchonetLiteManager.deviceList[i].status[0x80.toByte()] == 0x30.toByte()
-//            addData(i, mainText, subText, isOn)
-            data.add(ListItem(i, mainText, subText, isOn, if (isOn) "ON" else "OFF"))
-        }
-        println("bbbbb")
-        recycView.post {
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-
-    private fun checkData() {
-        lifecycleScope.launch {
-            for (i in 0..<data.size) {
-                val obj = EchonetLiteManager.deviceList[data[i].id]
-                val sent = obj.asyncGet("動作状態")
-                if (sent == null) {
-                    println("sent is null")
-                    continue
-                }
-                val ret = EchonetLiteManager.asyncWaitPacket(sent)
-                if (ret == null) {
-                    println("ret is null")
-                    continue
-                }
-                if (ret.edt.isNullOrEmpty()) {
-                    println("edt is null")
-                    continue
-                }
-                val edt = obj.edtToString(ret.epc, ret.edt)
-                updateData(i, edt == "true")
+class ElseLayout : DefaultLayout() {
+    override fun isTargetEoj(obj: EchonetLiteObject<Number>): Boolean {
+        val eojList =
+            listOf(listOf(0x01, 0x30), listOf(0x02, 0x90), listOf(0x02, 0x91), listOf(0x0E, 0xF0))
+        for (eoj in eojList) {
+            if (obj.compareEoj(eoj)) {
+                return false
             }
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_elect)
-//        val genebutton = findViewById<Button>(R.id.inc_button)
-        recycView = findViewById<RecyclerView>(R.id.rccv)
-        recycView.setHasFixedSize(true)
-        recycView.layoutManager = LinearLayoutManager(this).apply {
-            orientation = LinearLayoutManager.VERTICAL
-        }
-        adapter = ListAdapter(data, this)
-        recycView.adapter = adapter
-
-
-        lifecycleScope.launch {
-            EchonetLiteManager.asyncGetDeviceList()
-            showData()
-            println("data:${data.size}件")
-            println(data)
-            checkData()
-        }
-
-    }
-
-    override fun onSwitchClick(position: Int, isChecked: Boolean) {
-        println("on press")
-        fun reset() {
-            updateData(position, data[position].Switch)
-        }
-
-        val id = data[position].id
-        val obj = EchonetLiteManager.deviceList[id]
-        lifecycleScope.launch {
-            // edt:"true" or "false"
-            val set = obj.asyncSetC("動作状態", isChecked.toString())
-            if (set == null) {
-                reset()
-                return@launch
-            }
-            EchonetLiteManager.asyncWaitPacket(set)
-
-            val get = obj.asyncGet("動作状態")
-            if (get == null) {
-                reset()
-                return@launch
-            }
-
-            val ret = EchonetLiteManager.asyncWaitPacket(get)
-            if (ret?.edt == null || ret.edt.isEmpty()) {
-                reset()
-                return@launch
-            }
-
-            println("this is ${obj.edtToString(ret.epc, ret.edt)}")
-            if (obj.edtToString(ret.epc, ret.edt) == isChecked.toString()) {
-                // ちゃんと返答が返ってきてるなら変更
-                updateData(position, isChecked)
-            } else {
-                reset()
-            }
-        }
-    }
-
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        super.onSaveInstanceState(savedInstanceState)
-        savedInstanceState.putParcelableArrayList("data", ArrayList<Parcelable>(data))
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        data.clear()
-        data.addAll(savedInstanceState.getParcelableArrayList<ListItem>("data")!!)
-        adapter.notifyDataSetChanged()
+        return true
     }
 }
